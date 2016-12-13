e55e67db6eb8f7f3fd028c0d6b6ab6f899384c99
oxnoctisxo <tip.hon@hotmail.fr>
2016-12-13 10:37:47 +0100

    Revert "Initialisation"
    This reverts commit b31f84b8d2b9e362367a4fa9df0bf2a9d3d1eb9b.

--- Assets/Scripts/Behaviours/AbstractBehaviour.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class AbstractBehaviour : ScriptableObject {
-
-	public abstract void Execute (GameObject targetObject);
-
-}
--- Assets/Scripts/DieBehaviours/AbstractDieBehaviour.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class AbstractDieBehaviour : MonoBehaviour {
-
-	public abstract void Die (GameObject deadObject);
-
-}
